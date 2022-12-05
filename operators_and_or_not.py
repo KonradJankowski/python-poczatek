a1dc4b0a0b111c377da5ef8fe551fd21e67153be
Konrad Jankowski <elvonet@gmail.com>
2022-12-05 18:15:13 +0100

    Added code about running for president of the USA

--- operators_and_or_not.py
+++ operators_and_or_not.py
@@ -9,3 +9,26 @@ if name_lenght >=5:
         print(f"{name} to imię zwykłej długości!")
     else:
         print(f"{name} to długie imię")
+
+
+#=============================================================================
+
+born_as_usa_citizen_answear = input("Czy jesteś obywatelem USA od urodzenia ? (T/N) ")
+age = int(input("Ile masz lat? "))
+usa_residence_years = int(input("Ile lat mieszkasz w USA? "))
+
+if born_as_usa_citizen_answear == "T":
+    born_as_usa_citizen = True
+else:
+    born_as_usa_citizen = False
+
+if born_as_usa_citizen:
+    if age >= 35:
+        if usa_residence_years >= 14:
+            print("Możesz kandydować")
+        else:
+            print("Nie możesz kandydować")
+    else:
+        print("Nie możesz kandydować")
+else:
+    print("Nie możesz kandydować")
