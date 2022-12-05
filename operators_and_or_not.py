7776e455e7b0442e3abe9398ef48244feee70c5a
Konrad Jankowski <elvonet@gmail.com>
2022-12-05 18:28:21 +0100

    Refactor cod

--- operators_and_or_not.py
+++ operators_and_or_not.py
@@ -5,7 +5,7 @@ name_lenght = len(name)
 if name_lenght < 5:
     print(f"{name} to raczej krótkie imię")
 if name_lenght >=5:
-    if name_lenght <= 8:
+    if name_lenght <= 5 and name_lenght <=8:
         print(f"{name} to imię zwykłej długości!")
     else:
         print(f"{name} to długie imię")
