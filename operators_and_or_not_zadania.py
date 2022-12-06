ce007498e27b6fdf7a3d955967f70d76d4d7e9c6
Konrad Jankowski <elvonet@gmail.com>
2022-12-06 09:55:05 +0100

    Add new version 1.1

--- operators_and_or_not_zadania.py
+++ operators_and_or_not_zadania.py
@@ -1,7 +1,13 @@
 income = float(input("Jaki jest miesięczny przychód Twojej firmy?"))
 number_of_employees = int(input("Ilu pracowników zxatrudniasz? "))
+suppot_answear = input("Czy Twoja firma otrzymała juz jakieś wsparcie wcześniej ? (T/N)")
 
-if income < 15500 or (number_of_employees >= 3 and income < 30000 ):
+if suppot_answear == "T":
+    suppot_used = True
+else:
+    suppot_used = False
+
+if income < 15500 or (number_of_employees >= 3 and income < 30000 ) or (suppot_used not True) :
     print(f"Możesz otrzymać dotację")
 else:
     print("Niestety nie otrzymasz dotacji")
