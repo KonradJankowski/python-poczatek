36bbd7667f1338ac01c21971d215fff5b7ee3d4d
Konrad Jankowski <elvonet@gmail.com>
2022-12-07 09:08:42 +0100

    Add nie chapter elif

---
+++ elif.py
@@ -0,0 +1,20 @@
+# Mamy 4 rodzaje wsparcia - od najlepszych do najgorszych śa to:
+# - Główne wsparcie -> przychód poniżej 5000
+# - Wsparcie z funduszu pracowników -> od 5 do 10 pracowników
+# - Wsparcie dla nowych firm -> krócej niz 3 lata na rynku
+# - Wsparcie na pocieszenie -> dla każdego kto nie dostał żadnego innego
+
+income = 42000
+employees_number = 27
+years_on_the_market = 3
+
+if income  < 5000:
+    print("Przyznano główne wsparcie")
+else:
+    if 5 <= employees_number <= 10 :
+        print("Przyznano wsprarcie z funduszu pracowników")
+    else:
+        if years_on_the_market < 3:
+            print("Przyznano wsparcie dla nowych firm")
+        else:
+            print("Przyznano wsparcie na pocieszenie")
