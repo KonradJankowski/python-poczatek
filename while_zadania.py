c2cf6b5181fe4559e1e45d4e8c1eee1f637ee2f0
Konrad Jankowski <elvonet@gmail.com>
2022-12-09 08:52:52 +0100

    Add favorite shopping list

--- while_zadania.py
+++ while_zadania.py
@@ -25,3 +25,19 @@ while index < len(number):
 #    print(number_clear)
     index += 1
 print(number_clear)
+
+#####################################################################
+
+# Poproś użytkownika o podanie ulubionych dań, rozdzielając każde z nich przecinkiem
+
+# Nadstępnie wypisz każde z nich wraz z informacją, które miejsce na jej/jego liście.
+
+food = input("Podaj ulubioną listę dań, rozdzielając je przecinkiem: ")
+list = food.split(",")
+print(f"Lista jaką podałeś: {list}")
+index =0
+index_1 = 0
+while index < len(list):
+    index_1 +=1
+    print(f" Na pozycji {index_1} znajduje się {list[index]}")
+    index += 1
