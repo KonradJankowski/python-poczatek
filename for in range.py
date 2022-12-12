30887e742d7823a27c090fb242c24be2561996d3
Konrad Jankowski <elvonet@gmail.com>
2022-12-12 11:29:39 +0100

    Add for in rang - example

---
+++ for in range.py
@@ -0,0 +1,16 @@
+# Wypisywanie kolejnych liczb
+for number in range(4):
+    print(number)
+
+print("==========================================")
+#Zaczynamy od 2 do 5
+
+for number in range(2,6):
+    print(number) 
+
+print("==========================================")
+
+#Zaczynamy od 1 do 12 i co trzecia liczba
+
+for number in range(1,13, 3):
+    print(number)
