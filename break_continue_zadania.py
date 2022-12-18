ab9a052899e69ddeed1e8f719944f35137e8b451
Konrad Jankowski <elvonet@gmail.com>
2022-12-18 13:09:49 +0100

    Modifi  break_continue_zadania file

--- break_continue_zadania.py
+++ break_continue_zadania.py
@@ -20,9 +20,11 @@ else:
 print("=======================================================================")
 
 # Utwórz listę zawierającą różne liczby i skorzystaj z instrukcji continue aby wypisać tylko
+# przerywa pętle i przechodzi do ponownego wykonania pętli z pominięciem co jest dalej po continue
 numbers = [1, 3, 4 ,2, 3, 4, 56, 234, 2, 62, 523, 451, 34]
 
 for number in numbers:
     if number % 2 == 0:
         continue
     print(number)
+
